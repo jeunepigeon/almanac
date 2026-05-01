bab8288ab3e3ea00446f0decc3717b324d3348d4
unknown <emilio.wajnberg@gmail.com>
2026-05-01 17:40:10 +0200

    ADI via withDangerousMod plugin

--- plugins/with-adi-registration.js
+++ plugins/with-adi-registration.js
@@ -1,26 +1,18 @@
-const { withAndroidManifest } = require('@expo/config-plugins');
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function withADIRegistration(config) {
-  return withAndroidManifest(config, (config) => {
-    const manifest = config.modResults.manifest;
-
-    if (!manifest['meta-data']) {
-      manifest['meta-data'] = [];
-    }
-
-    const exists = manifest['meta-data'].find(
-      (item) => item.$['android:name'] === 'com.google.android.play.developerverfication.REGISTRATION_TOKEN'
-    );
-
-    if (!exists) {
-      manifest['meta-data'].push({
-        $: {
-          'android:name': 'com.google.android.play.developerverfication.REGISTRATION_TOKEN',
-          'android:value': 'CXGEUMV2VDBDKAAAAAAAAAAAAA',
-        },
-      });
-    }
-
-    return config;
-  });
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const assetsDir = path.join(config.modRequest.platformProjectRoot, 'app', 'src', 'main', 'assets');
+      fs.mkdirSync(assetsDir, { recursive: true });
+      fs.writeFileSync(
+        path.join(assetsDir, 'adi-registration.properties'),
+        'CXGEUMV2VDBDKAAAAAAAAAAAAA'
+      );
+      return config;
+    },
+  ]);
 };
